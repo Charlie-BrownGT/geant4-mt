9adcf765b4d894895429a6a65af6054ba3c1de5a
Guy Carter-Richardson <guy.carter-richardson@outlook.com>
2024-04-20 11:35:09 +0100

    adding mt functionality

--- sim.cc
+++ sim.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "G4RunManager.hh"
+#include "G4MTRunManager.hh"
 #include "G4UImanager.hh"
 #include "G4UIExecutive.hh"
 #include "G4VisManager.hh"
@@ -12,14 +13,19 @@
 
 int main(int argc, char** argv)
 {
-	G4RunManager *runManager = new G4RunManager();
+	G4UIExecutive *ui = 0;
+	
+	#ifdef G4MULTITHREADED
+		G4MTRunManager *runManager = new G4MTRunManager();
+	#else
+		G4RunManager *runManager = new G4RunManager();
+	#endif
+	
 	runManager->SetUserInitialization(new MyDetectorConstruction());
 	runManager->SetUserInitialization(new MyPhysicsList());
 	runManager->SetUserInitialization(new MyActionInitialization());
 	
-	runManager->Initialize();
-	
-	G4UIExecutive *ui = 0;
+	//runManager->Initialize();
 	
 	if(argc == 1)
 	{
